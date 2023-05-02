324992f23f662cf2af89f4b7626f47530970ee0a
Jatinvaghelaa <126477293+Jatinvaghelaa@users.noreply.github.com>
2023-05-02 11:01:16 +0530

    Update que11(6).c

--- ASSIGMENT/ASSIGMENT2/que11(6).c
+++ ASSIGMENT/ASSIGMENT2/que11(6).c
@@ -6,9 +6,6 @@ int main() {
    printf("Enter a number: ");
    scanf("%d", &num);
 
-   for(i = 1; i <= 10; i++) {
+   for(i = 1; i <= 10; i++) 
       printf("%d x %d = %d\n", num, i, num*i);
    }
-
-   return 0;
-}
