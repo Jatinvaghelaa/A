bb7026c3bfa773304692942a1f74e01c7ebc9483
Jatinvaghelaa <126477293+Jatinvaghelaa@users.noreply.github.com>
2023-05-06 18:05:41 +0530

    Add files via upload

---
+++ WORK/loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (){ 
+
+int rows =4
+int i, j;
+
+for(i = 1 ; i<= rows; i++){
+	for ( j = i; j <=i; j++){
+		printf("%d", i);{
+		printf("\n");
+		}
+	} 
+	
+} 
+	
+
+
+
+
+
